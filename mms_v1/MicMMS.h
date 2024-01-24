7864cd8789f4291b0b2b67e799f8eb93f0fb3d5e
ChavaP06 <62371038+ChavaP06@users.noreply.github.com>
2024-01-24 08:37:13 +0700

    Add files via upload

---
+++ mms_v1/MicMMS.h
@@ -0,0 +1,45 @@
+#ifndef MICMMS_H
+#define MICMMS_H
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include "ModbusRtu.h"
+#include <vector>
+#include <ArduinoJson.h>
+
+class MicMMS {
+private:
+  const char* ssid;
+  const char* password;
+  const char* mqtt_server;
+  int mqtt_port;
+  const char* mc_no;
+  WiFiClient wifiClient;
+  PubSubClient mqttClient;
+
+  int slaveId;
+  HardwareSerial& serialPort;
+  Modbus modbus;
+
+  IPAddress ip;
+  IPAddress gateway;
+  IPAddress subnet;
+public:
+  MicMMS(const char* ssid, const char* password, const char* mqtt_server, int mqtt_port, const char* mc_no, int slaveId, HardwareSerial& serialPort,const char* ip_address, const char* gateway_address, const char* subnet_mask);
+  
+  void init();
+  void reconnect();
+  void publishMessage(char* topic, const char* message);
+  void run();
+  void start();
+  std::vector<std::vector<String>>  get_def_tb();
+  void set_def_tb(std::vector<std::vector<String>> tableVector);
+  static void modbus_Task(void* pvParam);
+  static void func1_Task(void* pvParam);
+  static void func2_Task(void* pvParam);
+  static void func3_Task(void* pvParam);
+  static void esp_Task(void* pvParam);
+};
+
+#endif
